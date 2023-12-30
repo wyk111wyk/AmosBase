90cd514d8cf97100a53edbee163cdd3ba044380e
AmosFitness <wyk111wyk@me.com>
2023-12-30 11:43:03 +0800

    1.4.6
    - 增加小数点位数

--- Sources/Extension/Number+Extension.swift
+++ Sources/Extension/Number+Extension.swift
@@ -33,7 +33,7 @@ extension Int: Identifiable {
 public extension Double {
     /// 转换为文字
     ///
-    /// 默认不带小数点，最多3位小数点
+    /// 默认不带小数点，最多8位
     func toString(digit: Int = 0) -> String {
         if digit == 0 {
             return String(format: "%.0f", self)
@@ -41,8 +41,18 @@ public extension Double {
             return String(format: "%.1f", self)
         } else if digit == 2 {
             return String(format: "%.2f", self)
-        } else {
+        } else if digit == 3 {
             return String(format: "%.3f", self)
+        } else if digit == 4 {
+            return String(format: "%.4f", self)
+        } else if digit == 5 {
+            return String(format: "%.5f", self)
+        } else if digit == 6 {
+            return String(format: "%.6f", self)
+        } else if digit == 7 {
+            return String(format: "%.7f", self)
+        } else {
+            return String(format: "%.8f", self)
         }
     }
 }
